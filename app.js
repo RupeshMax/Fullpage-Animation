edb16889dc4297ac4b12b37ca25b16f06c16e358
Rubesh K R <rupesh.maxpain@gmail.com>
2021-01-28 19:19:12 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,25 @@
+new fullpage('#fullpage' ,{
+    autoscrolling: true,
+    navigation: true,
+    anchor: ['s1','s2','s3'],
+    navigationTooltips: ['Home', 'About us', 'Contact'],
+    responsiveHeight: 731,
+    responsiveWidth: 411,
+    onLeave: (origin,destination, direction) => {
+        const section = destination.item;
+        const title = section.querySelector("h1");
+        const tl = new TimelineMax({ delay: 0.5 });
+        tl.fromTo(title, 0.5, {y:"50", opacity:0}, {y: 0, opacity:1});
+
+        if(destination.index === 1) {
+            const chairs = document.querySelectorAll(".chair");
+            const description = document.querySelector(".description");
+
+            tl.fromTo(chairs, 0.7, {x: "100%" }, {x: "-39%"})
+            .fromTo(description, 0.5, {y:"50", opacity:0}, {y: 0, opacity:1})
+            .fromTo(chairs[0], 1, {opacity: 1}, {opacity: 1})
+            .fromTo(chairs[1], 1, {opacity: 0}, {opacity: 1})
+            .fromTo(chairs[2], 1, {opacity: 0}, {opacity: 1})
+        }
+    }
+});
